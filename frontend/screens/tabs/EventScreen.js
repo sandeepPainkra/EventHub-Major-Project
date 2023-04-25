d95baa49cf5a0c7fb32a0483f96da43c09cf6159
Sandeep <samdeeppainkra@gmail.com>
2023-04-26 01:40:39 +0530

    creating add event category section

--- frontend/screens/tabs/EventScreen.js
+++ frontend/screens/tabs/EventScreen.js
@@ -1,11 +1,23 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Modal,
+  Alert,
+  Pressable,
+  Button,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
 import Header from "../../common/Header";
 import { useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
 
 const EventScreen = () => {
-  // const navigation = useNavigation();
+  const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View className="flex-1 bg-[#1F1F39]">
       <Header
@@ -17,10 +29,62 @@ const EventScreen = () => {
       />
       <ScrollView className="h-full">
         <View className="flex-1 justify-center items-center py-9">
-          <Text className="text-white text-[25px] font-light">
-            All Event Programs
-          </Text>
-          <View className="w-[80%] h-[3px] bg-[#2F2F42]"></View>
+          <View className="w-[78%] flex flex-row items-center justify-between">
+            <Text className="text-white text-[25px] font-light">
+              All Event Programs
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Image
+                style={{ tintColor: "white" }}
+                className="w-[30px] h-[30px]"
+                source={require("../../assets/addition.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View className="w-[80%] h-[3px] bg-[#2F2F42] mt-2"></View>
+
+          {/* create Event category Model starts*/}
+          <View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+            >
+              <View className="flex-1 justify-center items-center bg-[#2F2F42] ">
+                <View className="w-[90%] bg-[#26264c] px-3 py-4 rounded-xl items-center ">
+                  <View>
+                    <Text className="text-[27px] text-gray-400 ">
+                      Create A Event Category
+                    </Text>
+                    <View>
+                      <TextInput
+                        className="text-[19px] px-3 py-2 placeholder-gray-500 text-gray-800 bg-[#d3d3da] rounded-[10px] mt-11  "
+                        placeholder="Title of Event Category"
+                      />
+                    </View>
+                  </View>
+                  <View className="w-[70%] mt-6 flex-row justify-between items-center">
+                    <TouchableOpacity className="w-[45%] bg-blue-400 py-2 flex justify-center rounded-lg items-center">
+                      <Text className="text-[22px] text-white ">Create</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => {
+                        setModalVisible(!modalVisible);
+                      }}
+                      className="w-[45%] bg-[#ed4b40] py-2 flex justify-center rounded-lg items-center"
+                    >
+                      <Text className="text-[22px] text-white ">Cancel</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </Modal>
+          </View>
+          {/* create Event category Model ends*/}
         </View>
         <View className="w-full h-auto px-6 flex-row flex-wrap justify-between">
           <TouchableOpacity className="relative w-[45%] h-[100px] justify-center item-center ">
